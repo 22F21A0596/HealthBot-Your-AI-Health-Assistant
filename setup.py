628ac2099208b9c7fcf580521be67705bb7b5423
22F21A0596 <makasisrilakshmi96@gmail.com>
2024-10-19 00:42:02 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = '22F21A0596',
+    author_email ='makasisrilakshmi96@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
